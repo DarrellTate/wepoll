9b897b41b2546ce187b1fa82934d14390bd93b3a
Tyler Horvat <kibur1986@gmail.com>
2018-01-13 05:16:03 -0700

    navbar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { PollsComponent } from './polls/polls.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UsersComponent } from './admin/users/users.component';
 import { AdminPollsComponent } from './admin/admin-polls/admin-polls.component';
-
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,12 +22,24 @@ import { AdminPollsComponent } from './admin/admin-polls/admin-polls.component';
     PollsComponent,
     ProfileComponent,
     UsersComponent,
-    AdminPollsComponent
+    AdminPollsComponent,
+    UsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, 
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'polls', component: PollsComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/admin-polls', component: AdminPollsComponent },
+      { path: 'admin/users', component: UsersComponent }
+      
+    ]),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
