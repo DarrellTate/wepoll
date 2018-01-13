707184c557ec6c23f7e122ef49826ee1b6948a2a
Tyler Horvat <kibur1986@gmail.com>
2018-01-13 04:29:16 -0700

    added navbar and components

---
+++ src/app/admin/admin-polls/admin-polls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-polls',
+  templateUrl: './admin-polls.component.html',
+  styleUrls: ['./admin-polls.component.css']
+})
+export class AdminPollsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PollsComponent } from './polls/polls.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './admin/users/users.component';
+import { AdminPollsComponent } from './admin/admin-polls/admin-polls.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    PollsComponent,
+    ProfileComponent,
+    UsersComponent,
+    AdminPollsComponent
   ],
   imports: [
     BrowserModule, 
@@ -19,3 +31,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
