d4a06c3d734fd6e38f957efb5e1f2f5ee3509482
Tyler Horvat <kibur1986@gmail.com>
2018-01-13 01:34:10 -0700

    added firebase, bootstrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCPmqGv2cBrCDEqpawQIm4BpYZt9CHRS6E",
+    authDomain: "wepoll-demo.firebaseapp.com",
+    databaseURL: "https://wepoll-demo.firebaseio.com",
+    projectId: "wepoll-demo",
+    storageBucket: "wepoll-demo.appspot.com",
+    messagingSenderId: "1020137025831"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCPmqGv2cBrCDEqpawQIm4BpYZt9CHRS6E",
+    authDomain: "wepoll-demo.firebaseapp.com",
+    databaseURL: "https://wepoll-demo.firebaseio.com",
+    projectId: "wepoll-demo",
+    storageBucket: "wepoll-demo.appspot.com",
+    messagingSenderId: "1020137025831"
+  }
 };
